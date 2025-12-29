d7f99e575537ea02977d33956a3e1c374467208d
Rasid Ekbal <rtechdevlopment123@gmail.com>
2025-12-29 22:18:13 +0530

    fix search result typo for category search

--- src/controller/search.ts
+++ src/controller/search.ts
@@ -8,8 +8,8 @@ const searchController=async(req:express.Request,res:express.Response)=>{
     if(!query)return res.status(400).json(new ApiError("please provide search target"));
     try {
         let wallpapers =await searchWallpaper(query);
-        let categorys=await searchCategories(query);
-       return res.status(200).json(new ApiResponse([{wallpapers},{categorys}]));
+        let categories=await searchCategories(query);
+       return res.status(200).json(new ApiResponse([{wallpapers},{categories}]));
     } catch (error) {
         console.log("error : ",error);
        return res.status(500).json(new ApiError(error));
